11da72455228030d94806d8fc5d9206026517528
Pete Woelfel <wafflespete@pm.me>
2022-03-23 17:22:02 -0500

    minor output syntax changes

--- setup.sh
+++ setup.sh
@@ -11,23 +11,34 @@ function error ()
 
 function install ()
 {
+
+	function obtain_psql_apt_key ()
+	{
+		echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list || error "Issue Occured while adding postgresql to sources.list.d"
+		wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - || error "Issue Occured while obtaining psql gpg key"
+	}
 	echo "Obtaining PSQL Apt Key"
 	echo "######################"
-	echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list || error "Issue Occured while adding postgresql to sources.list.d"
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - || error "Issue Occured while obtaining psql gpg key"
-
+	if obtain_psql_apt_key > $LOG 2>&1; then
+		echo "Apt Key Added
+		"
+	else
+		error "Issue Occured Obtaining PSQL Apt Key"
+	fi
 	echo "Getting Apt Update"
 	echo "###################"
-	if apt-get update > $LOG; then
-		echo "Update Successful"
+	if apt-get update > $LOG 2>&1; then
+		echo "Update Successful
+		"
 	else
 		error "Critical Issue Occured during apt-get update"
 	fi
 
 	echo "Installing Needed Packages"
 	echo "##########################"
-	if apt-get install -y perl python3 python3-pip  postgresql-12 postgresql-client-12 postgresql-plpython3-12  ansible swaks > $LOG; then
-		echo "Applications Installed"
+	if apt-get install -y perl python3 python3-pip  postgresql-12 postgresql-client-12 postgresql-plpython3-12  ansible swaks > $LOG 2>&1; then
+		echo "Applications Installed
+		"
 	else
 		error "Issue Occured Installing necessary applications"
 	fi
@@ -37,14 +48,14 @@ function setup_psql ()
 {
 	echo "Setting Up Database"
 	echo "###################"
-	cp  database/provisoner_schema.sql /var/lib/postgresql/schema.sql
+	cp  database/provisioner_schema.sql /var/lib/postgresql/schema.sql
 	if sudo -u postgres psql -f /var/lib/postgresql/schema.sql; then
-		echo "Database set up successfully!"
+		echo "Database set up successfully!
+		"
 	else
 		error "Critical: Failed to setup database"
 	fi
 	
-
 }
 
 install || exit 1
